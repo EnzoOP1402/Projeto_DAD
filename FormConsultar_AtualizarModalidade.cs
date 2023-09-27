ff148800498bf58df466300c5a29dbcf87814494
Enzo Olivato <enzopazian1402@gmail.com>
2023-09-27 08:01:25 -0300

    VersãoFinal - Final

--- FormConsultar_AtualizarModalidade.cs
+++ FormConsultar_AtualizarModalidade.cs
@@ -114,6 +114,10 @@ namespace Projeto_DuplinhaFeroz
                 {
                     btnAtivar.Enabled = true;
                 }
+                else
+                {
+                    btnAtivar.Enabled = false;
+                }
             }
         }
 
